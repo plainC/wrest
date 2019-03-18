59d0d9897ebfabc836915c8bf610848ebf4c3628
J.P. Iivonen <wondermacros@yahoo.com>
2019-03-18 18:09:13 +0200

    Update to new api.

--- wrest/rest.c
+++ wrest/rest.c
@@ -1,60 +1,60 @@
 #include "wrest.h"
 
 void
-rest_get(struct UVtcpServer* self, void* context, const char* uri,
+rest_get(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE GET: '%s'\n\n", uri);
+    printf("MODULE GET: '%s'\n\n", req->uri);
 
     *response = strdup("GET ok");
     *response_size = strlen(*response);
 }
 
 void
-rest_head(struct UVtcpServer* self, void* context, const char* uri,
+rest_head(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE HEAD: '%s'\n\n", uri);
+    printf("MODULE HEAD: '%s'\n\n", req->uri);
 
     *response = strdup("HEAD ok");
     *response_size = strlen(*response);
 }
 
 void
-rest_delete(struct UVtcpServer* self, void* context, const char* uri,
+rest_delete(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE DELETE: '%s'\n\n", uri);
+    printf("MODULE DELETE: '%s'\n\n", req->uri);
 
     *response = strdup("DELETE ok");
     *response_size = strlen(*response);
 }
 
 void
-rest_trace(struct UVtcpServer* self, void* context, const char* uri,
+rest_trace(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE TRACE: '%s'\n\n", uri);
+    printf("MODULE TRACE: '%s'\n\n", req->uri);
 
     *response = strdup("TRACE ok");
     *response_size = strlen(*response);
 }
 
 void
-rest_connect(struct UVtcpServer* self, void* context, const char* uri,
+rest_connect(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE TRACE: '%s'\n\n", uri);
+    printf("MODULE TRACE: '%s'\n\n", req->uri);
 
     *response = strdup("TRACE ok");
     *response_size = strlen(*response);
 }
 
 void
-rest_quit(struct UVtcpServer* self, void* context, const char* uri,
+rest_quit(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE QUIT: '%s'\n\n", uri);
+    printf("MODULE QUIT: '%s'\n\n", req->uri);
 
     *response = strdup("QUIT ok");
     *response_size = strlen(*response);
@@ -63,40 +63,40 @@ rest_quit(struct UVtcpServer* self, void* context, const char* uri,
 }
 
 void
-rest_post(struct UVtcpServer* self, void* context, const char* uri, char* body,
+rest_post(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE POST: '%s'\n\n", uri);
+    printf("MODULE POST: '%s'\n\n", req->uri);
 
     *response = strdup("POST ok");
     *response_size = strlen(*response);
 }
 
 void
-rest_put(struct UVtcpServer* self, void* context, const char* uri, char* body,
+rest_put(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE PUT: '%s'\n\n", uri);
+    printf("MODULE PUT: '%s'\n\n", req->uri);
 
     *response = strdup("PUT ok");
     *response_size = strlen(*response);
 }
 
 void
-rest_patch(struct UVtcpServer* self, void* context, const char* uri, char* body,
+rest_patch(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE PATCH: '%s'\n\n", uri);
+    printf("MODULE PATCH: '%s'\n\n", req->uri);
 
     *response = strdup("PATCH ok");
     *response_size = strlen(*response);
 }
 
 void
-rest_options(struct UVtcpServer* self, void* context, const char* uri, char* body,
+rest_options(struct UVtcpServer* self, void* context, struct wrest_http_req* req,
       char** response, size_t* response_size)
 {
-    printf("MODULE OPTIONS: '%s'\n\n", uri);
+    printf("MODULE OPTIONS: '%s'\n\n", req->uri);
 
     *response = strdup("OPTIONS ok");
     *response_size = strlen(*response);
